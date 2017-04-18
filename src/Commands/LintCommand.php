37fe12c1462b77b761337b618c432e7a69409de2
Logan Henson <logan@loganhenson.com>
2017-04-18 15:48:10 -0500

    initial usable state, adds tlint lint dir command and ignores vendor and public/index.php

--- src/Commands/LintCommand.php
+++ src/Commands/LintCommand.php
@@ -2,6 +2,9 @@
 
 namespace Tighten\Commands;
 
+use RecursiveDirectoryIterator;
+use RecursiveIteratorIterator;
+use RegexIterator;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
@@ -23,37 +26,67 @@ class LintCommand extends Command
             ->setHelp('This command allows you to lint a laravel file.');
 
         $this
-            ->addArgument('file', InputArgument::REQUIRED, 'The file to lint');
+            ->addArgument('file or directory', InputArgument::REQUIRED, 'The file or directory to lint');
     }
 
-    protected function execute(InputInterface $input, OutputInterface $output)
+    private function lintFile(InputInterface $input, OutputInterface $output, $file)
     {
-        if (!file_exists($input->getArgument('file'))) {
-            return $output->writeln('No file found at ' . $input->getArgument('file'));
-        }
-
-        $output->writeln([
-            'Linting ' . $input->getArgument('file'),
-            '============',
-        ]);
-
-        $linters = $this->getLinters($input->getArgument('file'));
+        $linters = $this->getLinters($file);
 
         $tighten = new TLint;
 
         $lints = [];
         foreach ($linters as $linter) {
-            $lints = array_merge($lints, $tighten->lint(new $linter(file_get_contents($input->getArgument('file')))));
+            $lints = array_merge($lints, $tighten->lint(new $linter(file_get_contents($file))));
         }
 
-        $output->writeln([
-            'Lints: ',
-            '============',
-        ]);
+        if (!empty($lints)) {
+            $output->writeln([
+                "Lints for $file: ",
+                '============',
+            ]);
 
-        foreach ($lints as $lint) {
-            $output->writeln((string) $lint);
+            foreach ($lints as $lint) {
+                $output->writeln((string) $lint);
+            }
         }
+    }
+
+    private function filesInDir($directory, $fileExtension)
+    {
+        $directory = realpath($directory);
+        $it = new \RecursiveDirectoryIterator($directory);
+        $it = new \RecursiveIteratorIterator($it, \RecursiveIteratorIterator::LEAVES_ONLY);
+        $it = new \RegexIterator($it, '(\.' . preg_quote($fileExtension) . '$)');
+
+        foreach ($it as $file) {
+            /** @var \SplFileObject $file */
+            $filepath = $file->getRealPath();
+
+            if (strpos($filepath, 'vendor') !== false
+            || strpos($filepath, 'public/index.php')) {
+                continue;
+            }
+
+            yield $filepath;
+        }
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        if (is_file($input->getArgument('file or directory'))) {
+            $this->lintFile($input, $output, $input->getArgument('file or directory'));
+        } elseif (is_dir($input->getArgument('file or directory'))) {
+            foreach ($this->filesInDir($input->getArgument('file or directory'), 'php') as $file) {
+                $this->lintFile($input, $output, $file);
+            }
+        } else {
+            $output->writeln('No file or directory found at ' . $input->getArgument('file or directory'));
+
+            return 1;
+        }
+
+        return 0;
     }
 
     private function getLinters($path)
