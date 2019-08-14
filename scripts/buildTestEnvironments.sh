2c970ae581d3efb8946b05ba4257774780d7a229
Logan Henson <logan@loganhenson.com>
2019-08-14 12:32:57 -0500

    Add 7.4 test harness

--- scripts/buildTestEnvironments.sh
+++ scripts/buildTestEnvironments.sh
@@ -9,5 +9,8 @@ docker build -t tightenco/tests-php7.2 ./environments/php7.2/
 echo "Building testing environment for PHP7.3..."
 docker build -t tightenco/tests-php7.3 ./environments/php7.3/
 
+echo "Building testing environment for PHP7.4..."
+docker build -t tightenco/tests-php7.4 ./environments/php7.4/
+
 echo "Building testing environment for PHP-Nightly..."
 docker build -t tightenco/tests-php-nightly ./environments/nightly/
--- scripts/runTests.sh
+++ scripts/runTests.sh
@@ -9,5 +9,8 @@ docker run -v $PWD:/home/tightenco/ tightenco/tests-php7.2
 echo "Running tests in PHP7.3..."
 docker run -v $PWD:/home/tightenco/ tightenco/tests-php7.3
 
+echo "Running tests in PHP7.4..."
+docker run -v $PWD:/home/tightenco/ tightenco/tests-php7.4
+
 echo "Running tests in PHP-Nightly..."
 docker run -v $PWD:/home/tightenco/ tightenco/tests-php-nightly
