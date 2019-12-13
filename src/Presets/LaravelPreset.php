d2d015d0d30b03b5f800ea570a00ed4ad978368f
Chris Morrell <inxilpro@users.noreply.github.com>
2019-12-13 16:15:29 -0500

    Use fully-qualified class names

--- src/Presets/LaravelPreset.php
+++ src/Presets/LaravelPreset.php
@@ -1,38 +1,65 @@
 <?php
 
 namespace Tighten\Presets;
+ 
+use Tighten\Linters\ApplyMiddlewareInRoutes;
+use Tighten\Linters\ArrayParametersOverViewWith;
+use Tighten\Linters\ClassThingsOrder;
+use Tighten\Linters\ImportFacades;
+use Tighten\Linters\MailableMethodsInBuild;
+use Tighten\Linters\ModelMethodOrder;
+use Tighten\Linters\NewLineAtEndOfFile;
+use Tighten\Linters\NoCompact;
+use Tighten\Linters\NoDd;
+use Tighten\Linters\NoLeadingSlashesOnRoutePaths;
+use Tighten\Linters\NoParensEmptyInstantiations;
+use Tighten\Linters\NoSpaceAfterBladeDirectives;
+use Tighten\Linters\NoStringInterpolationWithoutBraces;
+use Tighten\Linters\OneLineBetweenClassVisibilityChanges;
+use Tighten\Linters\PureRestControllers;
+use Tighten\Linters\QualifiedNamesOnlyForClassName;
+use Tighten\Linters\RemoveLeadingSlashNamespaces;
+use Tighten\Linters\RequestHelperFunctionWherePossible;
+use Tighten\Linters\RequestValidation;
+use Tighten\Linters\RestControllersMethodOrder;
+use Tighten\Linters\SpaceAfterBladeDirectives;
+use Tighten\Linters\SpaceAfterSoleNotOperator;
+use Tighten\Linters\SpacesAroundBladeRenderContent;
+use Tighten\Linters\TrailingCommasOnArrays;
+use Tighten\Linters\UseAuthHelperOverFacade;
+use Tighten\Linters\UseConfigOverEnv;
 
 class LaravelPreset implements PresetInterface
 {
     public function getLinters() : array
     {
         return [
-            'ApplyMiddlewareInRoutes',
-            'ArrayParametersOverViewWith',
-            'ClassThingsOrder',
-            'ImportFacades',
-            'MailableMethodsInBuild',
-            'ModelMethodOrder',
-            'NewLineAtEndOfFile',
-            'NoCompact',
-            'NoDd',
-            'NoLeadingSlashesOnRoutePaths',
-            'NoParensEmptyInstantiations',
-            'NoSpaceAfterBladeDirectives',
-            'NoStringInterpolationWithoutBraces',
-            'OneLineBetweenClassVisibilityChanges',
-            'PureRestControllers',
-            'QualifiedNamesOnlyForClassName',
-            'RemoveLeadingSlashNamespaces',
-            'RequestHelperFunctionWherePossible',
-            'RequestValidation',
-            'RestControllersMethodOrder',
-            'SpaceAfterBladeDirectives',
-            'SpaceAfterSoleNotOperator',
-            'SpacesAroundBladeRenderContent',
-            'TrailingCommasOnArrays',
-            'UseAuthHelperOverFacade',
-            'UseConfigOverEnv',
+            ApplyMiddlewareInRoutes::class,
+            ArrayParametersOverViewWith::class,
+            ClassThingsOrder::class,
+            ImportFacades::class,
+            MailableMethodsInBuild::class,
+            ModelMethodOrder::class,
+            NewLineAtEndOfFile::class,
+            NoCompact::class,
+            NoDd::class,
+            NoLeadingSlashesOnRoutePaths::class,
+            NoParensEmptyInstantiations::class,
+            NoSpaceAfterBladeDirectives::class,
+            NoStringInterpolationWithoutBraces::class,
+            OneLineBetweenClassVisibilityChanges::class,
+            PureRestControllers::class,
+            QualifiedNamesOnlyForClassName::class,
+            RemoveLeadingSlashNamespaces::class,
+            RequestHelperFunctionWherePossible::class,
+            RequestValidation::class,
+            RestControllersMethodOrder::class,
+            SpaceAfterBladeDirectives::class,
+            SpaceAfterSoleNotOperator::class,
+            SpacesAroundBladeRenderContent::class,
+            TrailingCommasOnArrays::class,
+            UseAuthHelperOverFacade::class,
+            UseConfigOverEnv::class,
         ];
     }
 
--- src/Presets/TightenPreset.php
+++ src/Presets/TightenPreset.php
@@ -2,51 +2,85 @@
 
 namespace Tighten\Presets;
 
+use Tighten\Linters\AlphabeticalImports;
+use Tighten\Linters\ApplyMiddlewareInRoutes;
+use Tighten\Linters\ArrayParametersOverViewWith;
+use Tighten\Linters\ClassThingsOrder;
+use Tighten\Linters\ConcatenationSpacing;
+use Tighten\Linters\ImportFacades;
+use Tighten\Linters\MailableMethodsInBuild;
+use Tighten\Linters\ModelMethodOrder;
+use Tighten\Linters\NewLineAtEndOfFile;
+use Tighten\Linters\NoCompact;
+use Tighten\Linters\NoDd;
+use Tighten\Linters\NoDocBlocksForMigrationUpDown;
+use Tighten\Linters\NoInlineVarDocs;
+use Tighten\Linters\NoLeadingSlashesOnRoutePaths;
+use Tighten\Linters\NoMethodVisibilityInTests;
+use Tighten\Linters\NoParensEmptyInstantiations;
+use Tighten\Linters\NoSpaceAfterBladeDirectives;
+use Tighten\Linters\NoStringInterpolationWithoutBraces;
+use Tighten\Linters\NoUnusedImports;
+use Tighten\Linters\OneLineBetweenClassVisibilityChanges;
+use Tighten\Linters\PureRestControllers;
+use Tighten\Linters\QualifiedNamesOnlyForClassName;
+use Tighten\Linters\RemoveLeadingSlashNamespaces;
+use Tighten\Linters\RequestHelperFunctionWherePossible;
+use Tighten\Linters\RequestValidation;
+use Tighten\Linters\RestControllersMethodOrder;
+use Tighten\Linters\SpaceAfterBladeDirectives;
+use Tighten\Linters\SpaceAfterSoleNotOperator;
+use Tighten\Linters\SpacesAroundBladeRenderContent;
+use Tighten\Linters\TrailingCommasOnArrays;
+use Tighten\Linters\UseAuthHelperOverFacade;
+use Tighten\Linters\UseConfigOverEnv;
+use Tighten\Linters\NoJsonDirective;
+
 class TightenPreset implements PresetInterface
 {
     public function getLinters() : array
     {
         return [
-            'AlphabeticalImports',
-            'ApplyMiddlewareInRoutes',
-            'ArrayParametersOverViewWith',
-            'ClassThingsOrder',
-            'ConcatenationSpacing',
-            'ImportFacades',
-            'MailableMethodsInBuild',
-            'ModelMethodOrder',
-            'NewLineAtEndOfFile',
-            'NoCompact',
-            'NoDd',
-            'NoDocBlocksForMigrationUpDown',
-            'NoInlineVarDocs',
-            'NoLeadingSlashesOnRoutePaths',
-            'NoMethodVisibilityInTests',
-            'NoParensEmptyInstantiations',
-            'NoSpaceAfterBladeDirectives',
-            'NoStringInterpolationWithoutBraces',
-            'NoUnusedImports',
-            'OneLineBetweenClassVisibilityChanges',
-            'PureRestControllers',
-            'QualifiedNamesOnlyForClassName',
-            'RemoveLeadingSlashNamespaces',
-            'RequestHelperFunctionWherePossible',
-            'RequestValidation',
-            'RestControllersMethodOrder',
-            'SpaceAfterBladeDirectives',
-            'SpaceAfterSoleNotOperator',
-            'SpacesAroundBladeRenderContent',
-            'TrailingCommasOnArrays',
-            'UseAuthHelperOverFacade',
-            'UseConfigOverEnv',
-            'NoJsonDirective',
+            AlphabeticalImports::class,
+            ApplyMiddlewareInRoutes::class,
+            ArrayParametersOverViewWith::class,
+            ClassThingsOrder::class,
+            ConcatenationSpacing::class,
+            ImportFacades::class,
+            MailableMethodsInBuild::class,
+            ModelMethodOrder::class,
+            NewLineAtEndOfFile::class,
+            NoCompact::class,
+            NoDd::class,
+            NoDocBlocksForMigrationUpDown::class,
+            NoInlineVarDocs::class,
+            NoLeadingSlashesOnRoutePaths::class,
+            NoMethodVisibilityInTests::class,
+            NoParensEmptyInstantiations::class,
+            NoSpaceAfterBladeDirectives::class,
+            NoStringInterpolationWithoutBraces::class,
+            NoUnusedImports::class,
+            OneLineBetweenClassVisibilityChanges::class,
+            PureRestControllers::class,
+            QualifiedNamesOnlyForClassName::class,
+            RemoveLeadingSlashNamespaces::class,
+            RequestHelperFunctionWherePossible::class,
+            RequestValidation::class,
+            RestControllersMethodOrder::class,
+            SpaceAfterBladeDirectives::class,
+            SpaceAfterSoleNotOperator::class,
+            SpacesAroundBladeRenderContent::class,
+            TrailingCommasOnArrays::class,
+            UseAuthHelperOverFacade::class,
+            UseConfigOverEnv::class,
+            NoJsonDirective::class,
         ];
     }
 
     public function getFormatters() : array
     {
         return [
-            'AlphabeticalImports',
+            AlphabeticalImports::class,
         ];
     }
 }
