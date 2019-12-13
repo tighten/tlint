b636651ec1f0304e6bce640161efdd684dd06d2e
Chris Morrell <inxilpro@users.noreply.github.com>
2019-12-13 13:31:24 -0500

    Allow for overriding linter in subclass

--- src/Linters/ModelMethodOrder.php
+++ src/Linters/ModelMethodOrder.php
@@ -17,7 +17,7 @@ class ModelMethodOrder extends BaseLinter
     use IdentifiesModelMethodTypes;
     use IdentifiesExtends;
 
-    private const METHOD_ORDER = [
+    protected const METHOD_ORDER = [
         0 => 'relationship',
         1 => 'scope',
         2 => 'accessor',
