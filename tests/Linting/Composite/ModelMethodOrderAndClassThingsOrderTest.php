76c7362ce5abad8fa462785083dd7934d90ba8d6
Tom Witkowski <dev.gummibeer@gmail.com>
2020-09-09 22:53:06 +0200

    add example model by @adriaanzon https://github.com/tightenco/tlint/pull/166#issuecomment-688954788

--- tests/Linting/Composite/ModelMethodOrderAndClassThingsOrderTest.php
+++ tests/Linting/Composite/ModelMethodOrderAndClassThingsOrderTest.php
@@ -9,10 +9,28 @@ use Tighten\TLint;
 
 class ModelMethodOrderAndClassThingsOrderTest extends TestCase
 {
-    /** @test */
-    public function a_valid_sorted_model_passes_both_linters(): void
+    /**
+     * @test
+     * @dataProvider models
+     */
+    public function a_valid_sorted_model_passes_both_linters(string $file): void
     {
-        $file = <<<PHP
+        $lints = [];
+        $lints += (new TLint)->lint(
+            new ModelMethodOrder($file),
+        );
+        $lints += (new TLint)->lint(
+            new ClassThingsOrder($file),
+        );
+
+        $this->assertEmpty($lints);
+    }
+
+    public function models(): array
+    {
+        return [
+            [
+                <<<PHP
 <?php
 
 namespace App;
@@ -51,16 +69,78 @@ class Post extends Model
 
     protected function drafting(){}
 }
-PHP;
+PHP
+            ],
+            [
+                <<<PHP
+<?php
 
-        $lints = [];
-        $lints += (new TLint)->lint(
-            new ModelMethodOrder($file),
-        );
-        $lints += (new TLint)->lint(
-            new ClassThingsOrder($file),
-        );
+namespace App\Models;
 
-        $this->assertEmpty($lints);
+class Song extends Model implements HasMedia
+{
+    use Actionable;
+    use HasTranslatableSections;
+    use InteractsWithMedia;
+    use Searchable;
+    use Sluggable;
+
+    protected \$appends = ['slug', 'title_with_translation'];
+
+    protected \$guarded = [];
+
+    protected \$hidden = ['internal_note'];
+
+    protected static function boot()
+    {
+    }
+
+    public function culture(): BelongsTo
+    {
+    }
+
+    public function tenants(): BelongsToMany
+    {
+    }
+
+    public function sections(): HasMany
+    {
+    }
+
+    public function scopeNotOnCurrentTenant(Builder \$query): void
+    {
+    }
+
+    public function scopePublished(Builder \$query): void
+    {
+    }
+
+    public function getTitleWithTranslationAttribute()
+    {
+    }
+
+    public function loadOrderedTranslatedSections(): self
+    {
+    }
+
+    public function url(): string
+    {
+    }
+
+    public function registerMediaCollections(): void
+    {
+    }
+
+    public function toSearchableArray(): array
+    {
+    }
+
+    public function shouldBeSearchable(): bool
+    {
+    }
+}
+PHP
+            ],
+        ];
     }
 }
