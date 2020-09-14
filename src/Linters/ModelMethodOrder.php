dc30a81f95e37432f2aab4bdf20c8f3b3297f6e6
Tom Witkowski <dev.gummibeer@gmail.com>
2020-09-14 16:10:50 +0200

    detect relationships last as they are the only ones without naming constraints

--- src/Linters/ModelMethodOrder.php
+++ src/Linters/ModelMethodOrder.php
@@ -48,10 +48,10 @@ class ModelMethodOrder extends BaseLinter
             // declare everything custom that's not public
             'custom' => Closure::fromCallable([$this, 'isCustomMethod']),
             // detect all methods that have to be public
-            'relationship' => Closure::fromCallable([$this, 'isRelationshipMethod']),
             'scope' => Closure::fromCallable([$this, 'isScopeMethod']),
             'accessor' => Closure::fromCallable([$this, 'isAccessorMethod']),
             'mutator' => Closure::fromCallable([$this, 'isMutatorMethod']),
+            'relationship' => Closure::fromCallable([$this, 'isRelationshipMethod']),
         ];
     }
 
