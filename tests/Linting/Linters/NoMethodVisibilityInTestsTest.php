fef17f08d9484e38365910109206eaa7f49c0e6e
peter279k <peter279k@gmail.com>
2020-12-04 23:19:12 +0800

    Improve PHPUnit fixture

--- tests/Linting/Linters/NoMethodVisibilityInTestsTest.php
+++ tests/Linting/Linters/NoMethodVisibilityInTestsTest.php
@@ -148,7 +148,7 @@ use PHPUnit\Framework\TestCase;
 
 class NoMethodVisibilityInTestsTest extends TestCase
 {
-    public function setUp() : void
+    protected function setUp() : void
     {
 
     }
