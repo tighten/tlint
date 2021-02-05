c57a1c7d2b26103d8371aac1bc92d22f6d19dee0
Jacob Baker-Kretzmar <jacobtbk@gmail.com>
2021-02-05 13:01:03 -0500

    Install illuminate/view and override constructor, remove custom implementation

---
+++ src/Illuminate/BladeCompiler.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tighten\Illuminate;
+
+use Illuminate\View\Compilers\BladeCompiler as IlluminateBladeCompiler;
+use InvalidArgumentException;
+
+class BladeCompiler extends IlluminateBladeCompiler
+{
+    public function __construct($files, $cachePath)
+    {
+        if (! $cachePath) {
+            throw new InvalidArgumentException('Please provide a valid cache path.');
+        }
+
+        $this->files = $files;
+        $this->cachePath = $cachePath;
+    }
+}
