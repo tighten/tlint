30abb7ac8769aab2307387c2540ff3fdf2c4df3a
Jacob Baker-Kretzmar <jacobtbk@gmail.com>
2021-02-12 01:17:33 -0500

    Add several failing tests

---
+++ tests/Linting/Linters/NoDatesPropertyOnModelsTest.php
@@ -0,0 +1,139 @@
+<?php
+
+namespace testing\Linting\Linters;
+
+use PHPUnit\Framework\TestCase;
+use Tighten\Linters\NoDatesPropertyOnModels;
+use Tighten\TLint;
+
+class NoDatesPropertyOnModelsTest extends TestCase
+{
+    /** @test */
+    function lints_dates_property_on_model()
+    {
+        $file = <<<'file'
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    protected $dates = ['published_at'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
+file;
+
+        $lints = (new TLint)->lint(new NoDatesPropertyOnModels($file));
+
+        $this->assertEquals(9, $lints[0]->getNode()->getLine());
+    }
+
+    /** @test */
+    function lints_dates_property_on_pivot_model()
+    {
+        $file = <<<'file'
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Relations\Pivot;
+
+class AuthorPost extends Pivot
+{
+    public $incrementing = false;
+
+    protected $dates = ['reviewed_at'];
+}
+file;
+
+        $lints = (new TLint)->lint(new NoDatesPropertyOnModels($file));
+
+        $this->assertEquals(11, $lints[0]->getNode()->getLine());
+    }
+
+    /** @test */
+    function lints_dates_property_on_authenticatable()
+    {
+        $file = <<<'file'
+<?php
+
+namespace App\Models;
+
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    protected $fillable = ['name', 'email'];
+    protected $dates = ['reviewed_at'];
+}
+file;
+
+        $lints = (new TLint)->lint(new NoDatesPropertyOnModels($file));
+
+        $this->assertEquals(13, $lints[0]->getNode()->getLine());
+    }
+
+    /** @test */
+    function lints_dates_property_with_custom_base_model()
+    {
+        $file = <<<'file'
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model as BaseModel;
+
+class Post extends BaseModel
+{
+    protected $dates = ['published_at'];
+}
+file;
+
+        $lints = (new TLint)->lint(new NoDatesPropertyOnModels($file));
+
+        $this->assertEquals(9, $lints[0]->getNode()->getLine());
+    }
+
+    /** @test */
+    function doesnt_lint_dates_property_on_non_model()
+    {
+        $file = <<<'file'
+<?php
+
+namespace App\Events;
+
+class Birthday
+{
+    protected $dates = ['date'];
+}
+file;
+
+        $this->assertEmpty((new TLint)->lint(new NoDatesPropertyOnModels($file)));
+    }
+
+    /** @test */
+    function doesnt_lint_dates_property_on_non_model_called_model()
+    {
+        $file = <<<'file'
+<?php
+
+namespace App\Items;
+
+class Ferrari extends Model
+{
+    protected $dates = ['manufactured_on'];
+}
+file;
+
+        $this->assertEmpty((new TLint)->lint(new NoDatesPropertyOnModels($file)));
+    }
+}
