9109ea3d22b03be723241d7ea663585b8699ce54
Jacob Baker-Kretzmar <jacobtbk@gmail.com>
2021-09-17 11:40:55 -0400

    Refactor checks for comments and blank lines

--- src/Linters/OneLineBetweenClassVisibilityChanges.php
+++ src/Linters/OneLineBetweenClassVisibilityChanges.php
@@ -21,26 +21,50 @@ class OneLineBetweenClassVisibilityChanges extends BaseLinter
 
         $visitor = new FindingVisitor(function (Node $node) use (&$notSeparatedByBlankLine) {
             if ($node instanceof Class_) {
-                $prev = null;
+                $previousNode = null;
 
-                foreach (array_filter($node->stmts, function ($stmt) {
-                    return in_array(get_class($stmt), [
-                        Node\Stmt\ClassConst::class,
-                        Node\Stmt\Property::class,
-                    ]);
-                }) as $stmt) {
-                    if (! is_null($prev)) {
-                        // dump($stmt);
-                        if ($prev->flags !== $stmt->flags &&
-                            (($stmt->getStartLine() - $prev->getEndLine() !== 2 && ! (bool) $stmt->getDocComment()  && empty($stmt->getComments())) ||
-                            ((bool) $stmt->getDocComment() && $stmt->getDocComment()->getStartLine() - $prev->getEndLine() !== 2 ) ||
-                            (! empty($stmt->getComments()) && ($stmt->getComments()[0]->getStartLine() - $prev->getEndLine() !== 2 && $stmt->getStartLine() - $stmt->getComments()[0]->getEndLine() !== 2)))
-                            ) {
-                            $notSeparatedByBlankLine[] = $stmt;
+                $constAndPropertyNodes = array_filter($node->stmts, function ($stmt) {
+                    return in_array(get_class($stmt), [Node\Stmt\ClassConst::class, Node\Stmt\Property::class]);
+                });
+
+                foreach ($constAndPropertyNodes as $node) {
+                    // Ignore the very first const/property
+                    if (is_null($previousNode)) {
+                        $previousNode = $node;
+                        continue;
+                    }
+
+                    // Ignore nodes with exactly the same visibility
+                    if ($previousNode->flags === $node->flags) {
+                        $previousNode = $node;
+                        continue;
+                    }
+
+                    // Ignore nodes separated by exactly one blank line and no comments
+                    if ($node->getStartLine() - $previousNode->getEndLine() === 2 && empty($node->getComments())) {
+                        $previousNode = $node;
+                        continue;
+                    }
+
+                    if (! empty($comments = $node->getComments())) {
+                        // Get all lines between these nodes that are part of any comment
+                        $commentLines = array_merge(...array_map(function ($comment) {
+                            return range($comment->getStartLine(), $comment->getEndLine());
+                        }, $comments));
+
+                        // Get all lines between these nodes
+                        $allLinesBetweenNodes = range($previousNode->getEndLine() + 1, $node->getStartLine() - 1);
+
+                        // Ignore nodes separated by comments when there is exactly one blank line within the comments
+                        if (count(array_diff($allLinesBetweenNodes, $commentLines)) === 1) {
+                            $previousNode = $node;
+                            continue;
                         }
                     }
 
-                    $prev = $stmt;
+                    $notSeparatedByBlankLine[] = $node;
+
+                    $previousNode = $node;
                 }
             }
 
