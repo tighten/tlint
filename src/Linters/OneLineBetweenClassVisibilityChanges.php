fcf63cceaea0d5f9dca7f2a07da3deb00866f8e8
Jacob Baker-Kretzmar <jacobtbk@gmail.com>
2021-09-17 11:46:22 -0400

    Comments

--- src/Linters/OneLineBetweenClassVisibilityChanges.php
+++ src/Linters/OneLineBetweenClassVisibilityChanges.php
@@ -28,7 +28,7 @@ class OneLineBetweenClassVisibilityChanges extends BaseLinter
                 });
 
                 foreach ($constAndPropertyNodes as $node) {
-                    // Ignore the very first const/property
+                    // Ignore the very first node
                     if (is_null($previousNode)) {
                         $previousNode = $node;
                         continue;
@@ -47,14 +47,15 @@ class OneLineBetweenClassVisibilityChanges extends BaseLinter
                     }
 
                     if (! empty($comments = $node->getComments())) {
-                        // Get all lines between these nodes that are part of any comment
+                        // Get the line numbers of all lines between this node and the previous one
+                        $allLinesBetweenNodes = range($previousNode->getEndLine() + 1, $node->getStartLine() - 1);
+
+                        // Get the line numbers of all lines between this node and the previous one that are part of any comment
                         $commentLines = array_merge(...array_map(function ($comment) {
                             return range($comment->getStartLine(), $comment->getEndLine());
                         }, $comments));
 
-                        // Get all lines between these nodes
-                        $allLinesBetweenNodes = range($previousNode->getEndLine() + 1, $node->getStartLine() - 1);
-
+                        // Diff <all the lines> and <the commented lines> to find the lines between these two nodes that are blank
                         // Ignore nodes separated by comments when there is exactly one blank line within the comments
                         if (count(array_diff($allLinesBetweenNodes, $commentLines)) === 1) {
                             $previousNode = $node;
