3cd4193bc71bb835002c8383c903c90f4f972a99
Jacob Baker-Kretzmar <jacobtbk@gmail.com>
2021-09-17 11:38:38 -0400

    Add test

--- tests/Linting/Linters/OneLineBetweenClassVisibilityChangesTest.php
+++ tests/Linting/Linters/OneLineBetweenClassVisibilityChangesTest.php
@@ -201,4 +201,34 @@ file;
 
         $this->assertEmpty($lints);
     }
+
+    /** @test */
+    function ignores_many_comments_between_visibility_changes()
+    {
+        $file = <<<'file'
+<?php
+
+namespace App;
+
+class Thing
+{
+    protected const OK = 1;
+    // public const NOT_OK = 2;
+
+    // another one
+    /**
+     * docblock!
+     */
+    // Hi there
+    //
+    private $ok;
+}
+file;
+
+        $lints = (new TLint)->lint(
+            new OneLineBetweenClassVisibilityChanges($file)
+        );
+
+        $this->assertEmpty($lints);
+    }
 }
