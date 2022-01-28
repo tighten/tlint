423aaa52c4ad0c89e8d0a5f0b5e2157f4fcdc41f
Jacob Baker-Kretzmar <jacobtbk@gmail.com>
2022-01-28 13:57:47 -0500

    Add shared traverser/visitor setup to base `Linter` class

--- src/BaseLinter.php
+++ src/BaseLinter.php
@@ -2,7 +2,11 @@
 
 namespace Tighten\TLint;
 
+use Closure;
+use LogicException;
 use PhpParser\Node;
+use PhpParser\NodeTraverser;
+use PhpParser\NodeVisitor\FindingVisitor;
 use PhpParser\Parser;
 
 class BaseLinter
@@ -29,7 +33,7 @@ class BaseLinter
 
     public function lint(Parser $parser)
     {
-        return [];
+        return $this->traverseAutomatically($parser);
     }
 
     public function getLintDescription()
@@ -71,5 +75,24 @@ class BaseLinter
     public function getFilename()
     {
         return $this->filename;
+    }
+
+    protected function visitor(): Closure
+    {
+        throw new LogicException('Custom linters must override either the `lint` or `visitor` method.');
+    }
+
+    protected function visitUsing(Parser $parser, Closure $callback): FindingVisitor
+    {
+        $traverser = new NodeTraverser;
+        $traverser->addVisitor($visitor = new FindingVisitor($callback));
+        $traverser->traverse($parser->parse($this->code));
+
+        return $visitor;
+    }
+
+    private function traverseAutomatically(Parser $parser): array
+    {
+        return $this->visitUsing($parser, $this->visitor())->getFoundNodes();
     }
 }
