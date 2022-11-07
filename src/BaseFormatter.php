847302f623991e4ae34ff0c0ae45ebbc5c787be3
Anthony Clark <anthonyclark@gmail.com>
2022-11-07 07:06:04 -0800

    Further extract lint/format base

--- src/BaseFormatter.php
+++ src/BaseFormatter.php
@@ -5,31 +5,11 @@ namespace Tighten\TLint;
 use PhpParser\Lexer;
 use PhpParser\Parser;
 
-class BaseFormatter
+class BaseFormatter extends Base
 {
-    public const DESCRIPTION = 'No Description for Formatter.';
-
-    protected $description;
-    protected $filename;
-    protected $code;
-    protected $codeLines;
-
-    public function __construct($code, $filename = null)
+    public function format(Parser $parser, Lexer $lexer): string
     {
-        $this->description = static::DESCRIPTION;
-        $this->filename = $filename;
-        $this->code = $code;
-        $this->codeLines = preg_split('/\r\n|\r|\n/', $code);
-    }
-
-    public static function appliesToPath(string $path, array $configPaths): bool
-    {
-        return true;
-    }
-
-    public function format(Parser $parser, Lexer $lexer)
-    {
-        return [];
+        return $this->code;
     }
 
     public function getFormatDescription()
@@ -40,32 +20,5 @@ class BaseFormatter
     public function setFormatDescription(string $description)
     {
         return $this->description = $description;
-    }
-
-    public function getFilename()
-    {
-        return $this->filename;
-    }
-
-    public function getCode(): string
-    {
-        return $this->code;
-    }
-
-    public function getCodeLines()
-    {
-        return $this->codeLines;
-    }
-
-    public function getCodeLine(int $line)
-    {
-        return $this->getCodeLines()[$line - 1];
-    }
-
-    public function replaceCodeLine(int $line, string $replacement): string
-    {
-        $this->codeLines[$line - 1] = $replacement;
-
-        return implode(PHP_EOL, $this->codeLines);
     }
 }
